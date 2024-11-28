d01e009afaf978b6d03ffbca5a387d88ba844b1a
Agatha Pires <agathapires11@outlook.com>
2024-11-28 00:49:53 -0300

    add shopping cart tests

---
+++ cypress/e2e/shopping-cart.cy.js
@@ -0,0 +1,57 @@
+describe('Carrinho de compras', () => {
+    beforeEach('', () => {
+      cy.visit('/')
+      cy.login('standard_user', 'secret_sauce')
+      cy.get('[data-test="title"]').should('be.visible')
+    })
+  
+    it('Verificar se produto foi adicionado corretamente ao carrinho', () => {
+      const productName = 'Sauce Labs Backpack'
+      const productId = 'sauce-labs-backpack'
+      cy.addToCart(productId)
+      cy.get('[data-test="shopping-cart-link"]').click()
+      cy.contains(productName).should('be.visible')
+    })
+
+    it('Verificar se produto foi removido corretamente do carrinho', () => {
+      const productName = 'Sauce Labs Bike Light'
+      const productId = 'sauce-labs-bike-light'
+      cy.addToCart(productId)
+      cy.get('[data-test="shopping-cart-link"]').click()
+      cy.removeFromCart(productId)
+      cy.contains(productName).should('not.exist')
+    })
+
+    it('Checkout com informações válidas', () => {
+      const productId = 'sauce-labs-bolt-t-shirt'
+      cy.addToCart(productId)
+      cy.get('[data-test="shopping-cart-link"]').click()
+      cy.fillCheckoutInformation('Test', 'User', '111111')
+      cy.contains('Checkout: Overview').should('be.visible')
+    })
+
+    it('Checkout com informações inválidas', () => {
+      const productId = 'sauce-labs-bolt-t-shirt'
+      cy.addToCart(productId)
+      cy.get('[data-test="shopping-cart-link"]').click()
+      cy.get('[data-test="checkout"]').click()
+      cy.get('[data-test="continue"]').click()
+      cy.get('.error-message-container').should('be.visible')
+    })
+
+    it('Verificar soma de valor do carrinho e frete', () => {
+      const productId = 'sauce-labs-backpack'
+      const secondProductId = 'sauce-labs-bike-light'
+      const productPrice = 29.99
+      const secondProductPrice = 9.99
+      cy.addToCart(productId)
+      cy.addToCart(secondProductId)
+      cy.get('[data-test="shopping-cart-link"]').click()
+      cy.fillCheckoutInformation('Test', 'User', '111111')
+      cy.get('[data-test="tax-label"]').invoke('text').then((taxText) => { 
+        const taxPrice = parseFloat(taxText.replace('Tax: $', ''))
+        const totalPrice = taxPrice + productPrice + secondProductPrice
+        cy.get('[data-test="total-label"]').should('have.text', `Total: $${totalPrice}`)
+      })
+    })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,18 @@
-Cypress.Commands.add('login', (email, senha) => {
-    cy.get('[data-test="username"]').type(email);
-    cy.get('[data-test="password"]').type(senha);
-    cy.get('[data-test="login-button"]').click();
- });
+Cypress.Commands.add('login', (email, password) => {
+    cy.get('[data-test="username"]').type(email)
+    cy.get('[data-test="password"]').type(password)
+    cy.get('[data-test="login-button"]').click()
+});
+Cypress.Commands.add('addToCart', (product) => {
+    cy.get(`[data-test="add-to-cart-${product}"]`).click()
+});
+Cypress.Commands.add('removeFromCart', (product) => {
+    cy.get(`[data-test="remove-${product}"]`).click()
+})
+Cypress.Commands.add('fillCheckoutInformation', (firstName, lastName, postalCode) => {
+    cy.get('[data-test="checkout"]').click()
+    cy.get('[data-test="firstName"]').type(firstName)
+    cy.get('[data-test="lastName"]').type(lastName)
+    cy.get('[data-test="postalCode"]').type(postalCode)
+    cy.get('[data-test="continue"]').click()
+})
