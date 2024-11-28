7ea9bb1dfbf374e7b7f7b5978dd79a593946a93d
Agatha Pires <agathapires11@outlook.com>
2024-11-27 22:24:14 -0300

    remove unnecessary semicolons

--- cypress/e2e/login-page.cy.js
+++ cypress/e2e/login-page.cy.js
@@ -1,13 +1,13 @@
 describe('Página de login', () => {
   beforeEach('', () => {
     cy.visit("https://www.saucedemo.com");
-    cy.contains('Swag Labs').should('be.visible');
+    cy.contains('Swag Labs').should('be.visible')
   })
 
   it('Login com credenciais válidas', () => {
-    cy.login('standard_user', 'secret_sauce');
+    cy.login('standard_user', 'secret_sauce')
     cy.contains('Products').should('be.visible')
-  });
+  })
 
   it('Login com senha inválida', () => {
     cy.login('standard_user', 'wrong_password')
@@ -16,15 +16,15 @@ describe('Página de login', () => {
   })
 
   it('Tentativa de login sem inserir usuário', () => {
-    cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="login-button"]').click()
     cy.contains('Epic sadface: Username is required')
       .should('be.visible')
   })
 
   it('Tentativa de login sem inserir senha', () => {
-    cy.get('[data-test="username"]').type('standard_user');
-    cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="login-button"]').click()
     cy.contains('Epic sadface: Password is required')
       .should('be.visible')
   })
-});
+})
